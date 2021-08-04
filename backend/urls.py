be210c67fca95b8ea9154237a2f73ba40bc5edf5
stesun2 <ss27@illinois.edu>
2021-08-03 19:15:50 -0500

    created simple url routing

---
+++ backend/urls.py
@@ -0,0 +1,9 @@
+import tasks
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('tasks.urls'))
+]
+
