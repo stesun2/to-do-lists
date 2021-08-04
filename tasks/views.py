540e938af32f3bb1412581da779284a239dbaf89
stesun2 <ss27@illinois.edu>
2021-08-04 02:03:10 -0500

    created model forms and create item, item not displaying when inserting new tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'tasks/list.html')
+from .models import *
+from .forms import *
 
+# Create your views here.
+
+def index(request):
+	tasks = Task.objects.all()
+
+	form = TaskForm()
+
+	if request.method =='POST':
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return redirect('/')
+
+
+	context = {'tasks':tasks, 'form':form}
+	return render(request, 'tasks/list.html', context)
